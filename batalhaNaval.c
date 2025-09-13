14112998615eddf84ab7a038fad69e79d3f1c8df
Niivas0 <nivaldo_lombardo@hotmail.com>
2025-09-13 17:45:50 +0000

    Criado o Código do arquivo "batalhaNaval.c" em 13/09/2025. Tema5 - Desafio - Aventureiro.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,44 +1,78 @@
 #include <stdio.h>
 
-int main() {
-    // Constantes para configuração do jogo
-    int tabuleiro[10][10];
-    int i, j;
+// Verifica se o navio pode ser posicionado sem sair do tabuleiro ou sobrepor outro
+int pode_posicionar(int tabuleiro[10][10], int linha, int coluna, int direcao, int diagonal) {
+    for (int i = 0; i < 3; i++) {
+        int l = linha, c = coluna;
 
-
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0;
+        if (diagonal == 1) {
+            l += i;
+            c += i;
+        } else if (diagonal == 2) {
+            l += i;
+            c -= i;
+        } else if (direcao == 0) {
+            c += i;
+        } else if (direcao == 1) {
+            l += i;
         }
+
+        if (l < 0 || l >= 10 || c < 0 || c >= 10 || tabuleiro[l][c] != 0)
+            return 0;
     }
+    return 1;
+}
 
+// Posiciona o navio no tabuleiro
+void posicionar_navio(int tabuleiro[10][10], int linha, int coluna, int direcao, int diagonal) {
+    for (int i = 0; i < 3; i++) {
+        int l = linha, c = coluna;
 
-    int linhaNavioHorizontal = 2;
-    int colunaNavioHorizontal = 4;
+        if (diagonal == 1) {
+            l += i;
+            c += i;
+        } else if (diagonal == 2) {
+            l += i;
+            c -= i;
+        } else if (direcao == 0) {
+            c += i;
+        } else if (direcao == 1) {
+            l += i;
+        }
 
-    int linhaNavioVertical = 5;
-    int colunaNavioVertical = 7;
-
-
-    // Posiciona navio horizontal
-    for (i = 0; i < 3; i++) {
-        tabuleiro[linhaNavioHorizontal][colunaNavioHorizontal + i] = 3;
+        tabuleiro[l][c] = 3;
     }
+}
 
-    // Posiciona navio vertical
-    for (i = 0; i < 3; i++) {
-        tabuleiro[linhaNavioVertical + i][colunaNavioVertical] = 3;
-    }
-
-
-
-    printf("\n=== TABULEIRO BATALHA NAVAL ===\n\n");
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
+// Exibe o tabuleiro no console
+void exibir_tabuleiro(int tabuleiro[10][10]) {
+    printf("=== TABULEIRO BATALHA NAVAL ===\n");
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
+}
+
+int main() {
+    int tabuleiro[10][10] = {0}; // Inicializa o tabuleiro com água
+
+    // Posiciona dois navios horizontais/verticais
+    if (pode_posicionar(tabuleiro, 0, 0, 0, 0))
+        posicionar_navio(tabuleiro, 0, 0, 0, 0);
+
+    if (pode_posicionar(tabuleiro, 5, 2, 1, 0))
+        posicionar_navio(tabuleiro, 5, 2, 1, 0);
+
+    // Posiciona dois navios diagonais
+    if (pode_posicionar(tabuleiro, 2, 2, 0, 1))
+        posicionar_navio(tabuleiro, 2, 2, 0, 1);
+
+    if (pode_posicionar(tabuleiro, 0, 9, 0, 2))
+        posicionar_navio(tabuleiro, 0, 9, 0, 2);
+
+    exibir_tabuleiro(tabuleiro);
 
     return 0;
 }
