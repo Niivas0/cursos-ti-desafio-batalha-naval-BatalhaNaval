622d781d0a25278041dc7342da16d080f89d5a06
Niivas0 <nivaldo_lombardo@hotmail.com>
2025-09-13 18:12:59 +0000

    Criado o Código do arquivo "batalhaNaval.c" em 13/09/2025. Tema5 - Desafio - Mestre.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,52 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Verifica se o navio pode ser posicionado sem sair do tabuleiro ou sobrepor outro
-int pode_posicionar(int tabuleiro[10][10], int linha, int coluna, int direcao, int diagonal) {
-    for (int i = 0; i < 3; i++) {
-        int l = linha, c = coluna;
-
-        if (diagonal == 1) {
-            l += i;
-            c += i;
-        } else if (diagonal == 2) {
-            l += i;
-            c -= i;
-        } else if (direcao == 0) {
-            c += i;
-        } else if (direcao == 1) {
-            l += i;
-        }
-
-        if (l < 0 || l >= 10 || c < 0 || c >= 10 || tabuleiro[l][c] != 0)
-            return 0;
-    }
-    return 1;
-}
-
-// Posiciona o navio no tabuleiro
-void posicionar_navio(int tabuleiro[10][10], int linha, int coluna, int direcao, int diagonal) {
-    for (int i = 0; i < 3; i++) {
-        int l = linha, c = coluna;
-
-        if (diagonal == 1) {
-            l += i;
-            c += i;
-        } else if (diagonal == 2) {
-            l += i;
-            c -= i;
-        } else if (direcao == 0) {
-            c += i;
-        } else if (direcao == 1) {
-            l += i;
-        }
-
-        tabuleiro[l][c] = 3;
-    }
-}
-
-// Exibe o tabuleiro no console
+// Exibe o tabuleiro com água (0), navio (3) e área de habilidade (5)
 void exibir_tabuleiro(int tabuleiro[10][10]) {
-    printf("=== TABULEIRO BATALHA NAVAL ===\n");
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             printf("%d ", tabuleiro[i][j]);
@@ -55,23 +11,78 @@ void exibir_tabuleiro(int tabuleiro[10][10]) {
     }
 }
 
+// Aplica uma matriz de habilidade ao tabuleiro, centrando no ponto de origem
+void aplicar_habilidade(int tabuleiro[10][10], int habilidade[5][5], int origem_linha, int origem_coluna) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            int linha = origem_linha - 2 + i;
+            int coluna = origem_coluna - 2 + j;
+
+            if (linha >= 0 && linha < 10 && coluna >= 0 && coluna < 10) {
+                if (habilidade[i][j] == 1 && tabuleiro[linha][coluna] == 0) {
+                    tabuleiro[linha][coluna] = 5;
+                }
+            }
+        }
+    }
+}
+
+// Gera matriz de habilidade em forma de cone
+void gerar_cone(int matriz[5][5]) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == 0 && j == 2) matriz[i][j] = 1;
+            else if (i == 1 && (j >= 1 && j <= 3)) matriz[i][j] = 1;
+            else if (i == 2 && (j >= 0 && j <= 4)) matriz[i][j] = 1;
+            else matriz[i][j] = 0;
+        }
+    }
+}
+
+// Gera matriz de habilidade em forma de cruz
+void gerar_cruz(int matriz[5][5]) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == 2 || j == 2) matriz[i][j] = 1;
+            else matriz[i][j] = 0;
+        }
+    }
+}
+
+// Gera matriz de habilidade em forma de octaedro (losango)
+void gerar_octaedro(int matriz[5][5]) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (abs(i - 2) + abs(j - 2) <= 2) matriz[i][j] = 1;
+            else matriz[i][j] = 0;
+        }
+    }
+}
+
 int main() {
-    int tabuleiro[10][10] = {0}; // Inicializa o tabuleiro com água
+    int tabuleiro[10][10] = {0};
 
-    // Posiciona dois navios horizontais/verticais
-    if (pode_posicionar(tabuleiro, 0, 0, 0, 0))
-        posicionar_navio(tabuleiro, 0, 0, 0, 0);
+    // Posiciona navios fixos
+    tabuleiro[0][0] = 3;
+    tabuleiro[0][1] = 3;
+    tabuleiro[0][2] = 3;
+    tabuleiro[5][2] = 3;
+    tabuleiro[6][2] = 3;
+    tabuleiro[7][2] = 3;
 
-    if (pode_posicionar(tabuleiro, 5, 2, 1, 0))
-        posicionar_navio(tabuleiro, 5, 2, 1, 0);
+    // Matrizes de habilidades
+    int cone[5][5], cruz[5][5], octaedro[5][5];
 
-    // Posiciona dois navios diagonais
-    if (pode_posicionar(tabuleiro, 2, 2, 0, 1))
-        posicionar_navio(tabuleiro, 2, 2, 0, 1);
+    gerar_cone(cone);
+    gerar_cruz(cruz);
+    gerar_octaedro(octaedro);
 
-    if (pode_posicionar(tabuleiro, 0, 9, 0, 2))
-        posicionar_navio(tabuleiro, 0, 9, 0, 2);
+    // Aplica habilidades em pontos fixos
+    aplicar_habilidade(tabuleiro, cone, 2, 2);
+    aplicar_habilidade(tabuleiro, cruz, 5, 5);
+    aplicar_habilidade(tabuleiro, octaedro, 7, 7);
 
+    // Exibe resultado final
     exibir_tabuleiro(tabuleiro);
 
     return 0;
